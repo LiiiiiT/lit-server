29ca597dd4e76439a80aa07f1b649b4deca193a8
김경탁 <fandom-tak@gimgyeongtag-ui-MacBookPro.local>
2022-07-28 15:09:03 +0900

    :sparkles: Add health api

--- src/main/java/com/litserver/SampleApi.java
+++ src/main/java/com/litserver/SampleApi.java
@@ -1,17 +1,14 @@
 package com.litserver;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/sample")
+@RequestMapping("/api/sample")
 public class SampleApi {
 
-    @GetMapping
-    public ResponseEntity test(){
-        return new ResponseEntity("123", HttpStatus.OK);
+    @RequestMapping("/health")
+    public String health() {
+        return "OK";
     }
 }
