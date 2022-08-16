9e6f81b13fbb4f58dd7189431741f325f3b19197
김경탁 <tak6831@gmail.com>
2022-08-16 19:16:35 +0900

    :construction_worker: Add CICD

--- src/main/java/com/litserver/HealthCheckController.java
+++ src/main/java/com/litserver/HealthCheckController.java
@@ -4,6 +4,7 @@ import com.litserver.global.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class HealthCheckController {
 
-    @RequestMapping("/health-check")
+    @GetMapping("/health-check")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("OK");
     }
