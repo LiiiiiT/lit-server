6c689b4de86a3aebdbb902f5967a58bf26deb750
김경탁 <tak6831@gmail.com>
2022-10-07 17:28:02 +0900

    feat Add log

--- src/main/java/com/litserver/global/config/WebSecurityConfig.java
+++ src/main/java/com/litserver/global/config/WebSecurityConfig.java
@@ -1,40 +1,33 @@
 package com.litserver.global.config;
 
-
+import com.litserver.global.filter.AllowOriginFilter;
 import com.litserver.global.filter.XSSRequestFilter;
 import com.litserver.global.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
                 .httpBasic().disable()
                 .csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()//
                 // 권한 설정
-                .antMatchers("/v2/api-docs/**", "/swagger-resources/**", "/swagger-ui/**", "/swagger-ui.html","/webjars/**", "/swagger/**",   // swagger
+                .antMatchers("/v2/api-docs/**", "/swagger-resources/**", "/swagger-ui/**", "/swagger-ui.html", "/webjars/**", "/swagger/**",   // swagger
                         "/favicon.ico").hasIpAddress("127.0.0.1")
                 .antMatchers("/users/signin").permitAll()//
                 .antMatchers("/users/signup").permitAll()//
@@ -44,34 +37,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
 
         // If a user try to access a resource without having enough permissions
-        http.exceptionHandling().accessDeniedPage("/login");
+        httpSecurity.exceptionHandling().accessDeniedPage("/login");
         // Apply JWT
-        http.apply(new JwtTokenConfig(jwtTokenProvider));
+        httpSecurity.apply(new JwtTokenConfig(jwtTokenProvider));
 
-        // Optional, if you want to test the API from a browser
-        // http.httpBasic();
+        return httpSecurity.build();
     }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        // Allow swagger to be accessed without authentication
-        web.ignoring().antMatchers("/v2/api-docs")//
-                .antMatchers("/swagger-resources/**")//
-                .antMatchers("/swagger-ui.html")//
-                .antMatchers("/configuration/**")//
-                .antMatchers("/webjars/**")//
-                .antMatchers("/public");
+    @Bean
+    public FilterRegistrationBean<XSSRequestFilter> xssRequestFilterFilterRegistrationBean() {
+        FilterRegistrationBean<XSSRequestFilter> registration = new FilterRegistrationBean<>();
+        registration.setName("xssRequestFilter");
+        registration.setFilter(new XSSRequestFilter());
+        registration.addUrlPatterns("/*");
+        return registration;
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12);
+    public FilterRegistrationBean<AllowOriginFilter> allowOriginFilterFilterRegistrationBean() {
+        FilterRegistrationBean<AllowOriginFilter> registration = new FilterRegistrationBean<>();
+        registration.setName("AllowOriginFilter");
+        registration.setFilter(new AllowOriginFilter());
+        registration.addUrlPatterns("/*");
+        return registration;
     }
-
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
 }
